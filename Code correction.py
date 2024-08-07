0b99ed24c3185e985ae9d57ca7124c4af49fad5b
gopal1407 <gopal140791@yahoo.com>
2024-08-07 17:06:26 -0400

    first commit

---
+++ Code correction.py
@@ -0,0 +1,20 @@
+# Task 1
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")   
+
+
+# Task 2
+
+#how user feel
+mood = input("How do you feel today?")
+if mood.lower() == "happy":
+    print("That's great to hear!")
+if mood.lower() =="sad":
+    print("I hope your day gets better!")
+else:
+    print("I hope you have a good day!")   
